fe83f6a58204d74409ff7d45a318f614a4757b3f
georgioskaragiannis <gk13@hw.ac.uk>
2016-03-03 17:04:19 +0000

    test 2 giorgos

--- Restaurant Stage 2/src/Main.java
+++ Restaurant Stage 2/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("HELLO");
 		System.out.println("test2");
-			System.out.println("test4");
 			System.out.println("lalalal");
+			
+			
 	}
 
 }
